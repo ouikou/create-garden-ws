bb1134eec047ff85615c82d46dffb98f64ae7364
wang.weiguang <wang.weiguang@zipc.com>
2023-11-10 15:11:09 +0900

    first commit

--- packages/create-garden-ws/createGardenWs.js
+++ packages/create-garden-ws/createGardenWs.js
@@ -99,6 +99,23 @@ function init() {
             )
             .then(console.log);
     }
+
+    if (typeof projectName === 'undefined') {
+        console.error('Please specify the project directory:');
+        console.log(
+            `  ${chalk.cyan(program.name())} ${chalk.green('<project-directory>')}`
+        );
+        console.log();
+        console.log('For example:');
+        console.log(
+            `  ${chalk.cyan(program.name())} ${chalk.green('my-garden-ws')}`
+        );
+        console.log();
+        console.log(
+            `Run ${chalk.cyan(`${program.name()} --help`)} to see all options.`
+        );
+        process.exit(1);
+    }
 }
 function getTemplateInstallPackage() {
     console.log('############  getTemplateInstallPackage()')
