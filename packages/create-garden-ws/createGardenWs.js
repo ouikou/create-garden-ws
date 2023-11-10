f6f85fedb049c0aad630a78e87b6f819cdfd6ef4
wang.weiguang <wang.weiguang@zipc.com>
2023-11-10 15:07:42 +0900

    first commit

--- packages/create-garden-ws/createGardenWs.js
+++ packages/create-garden-ws/createGardenWs.js
@@ -29,10 +29,76 @@
 
 'use strict';
 
+const commander = require('commander');
+const chalk = require('chalk');
+
 const packageJson = require('./package.json');
 
+let projectName;
+
 function init() {
-    console.log('############  init()')
+    const program = new commander.Command(packageJson.name)
+        .version(packageJson.version)
+        .arguments('<project-directory>')
+        .usage(`${chalk.green('<project-directory>')} [options]`)
+        .action(name => {
+            projectName = name;
+        })
+        .option('--verbose', 'print additional logs')
+        .option('--info', 'print environment debug info')
+        .option(
+            '--template <path-to-template>',
+            'specify a template for the created workspace'
+        )
+        .allowUnknownOption()
+        .on('--help', () => {
+            console.log(
+                `    Only ${chalk.green('<project-directory>')} is required.`
+            );
+            console.log();
+            console.log(`    A custom ${chalk.cyan('--template')} can be one of:`);
+            console.log(
+                `      - a custom template published on npm: ${chalk.green(
+                    'cgw-template'
+                )}`
+            );
+            console.log(
+                `      - a local path relative to the current working directory: ${chalk.green(
+                    'file:../my-custom-template'
+                )}`
+            );
+            console.log();
+        })
+        .parse(process.argv);
+
+    if (program.info) {
+        console.log(chalk.bold('\nEnvironment Info:'));
+        console.log(
+            `\n  current version of ${packageJson.name}: ${packageJson.version}`
+        );
+        console.log(`  running from ${__dirname}`);
+        return envinfo
+            .run(
+                {
+                    System: ['OS', 'CPU'],
+                    Binaries: ['Node', 'npm', 'Yarn'],
+                    Browsers: [
+                        'Chrome',
+                        'Edge',
+                        'Internet Explorer',
+                        'Firefox',
+                        'Safari',
+                    ],
+                    npmPackages: ['react', 'react-dom', 'react-scripts'],
+                    npmGlobalPackages: ['create-react-app'],
+                },
+                {
+                    duplicates: true,
+                    showNotFound: true,
+                }
+            )
+            .then(console.log);
+    }
 }
 function getTemplateInstallPackage() {
     console.log('############  getTemplateInstallPackage()')
