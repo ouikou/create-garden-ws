364e7b3bb078365cf16aa4b89d269d08fc2711e8
wang.weiguang <wang.weiguang@zipc.com>
2023-11-13 12:34:08 +0900

    first commit

--- packages/create-garden-ws/createGardenWs.js
+++ packages/create-garden-ws/createGardenWs.js
@@ -38,6 +38,8 @@ const fs = require('fs-extra');
 const path = require('path');
 const semver = require('semver');
 const spawn = require('cross-spawn');
+const tmp = require('tmp');
+const unpack = require('tar-pack').unpack;
 
 
 const validateProjectName = require('validate-npm-package-name');
@@ -219,10 +221,17 @@ function run(
         getTemplateInstallPackage(template, originalDirectory),
     ]).then(([packageToInstall, templateToInstall]) => {
         const allDependencies = [packageToInstall];
-        console.log(`###################################packageToInstall: ${packageToInstall}`);
-        console.log(`###################################templateToInstall: ${templateToInstall}`);
 
         console.log('Installing packages. This might take a couple of minutes.');
+
+        Promise.all([
+            getPackageInfo(packageToInstall),
+            getPackageInfo(templateToInstall),
+        ])
+            .then(([packageInfo, templateInfo]) => {
+                console.log(`########################################packageInfo:  ${packageInfo}`);
+                console.log(`########################################packageInfo:  ${templateInfo}`);
+            })
     });
 }
 
@@ -293,6 +302,103 @@ function getTemplateInstallPackage(template, originalDirectory) {
     }
 
     return Promise.resolve(templateToInstall);
+}
+
+function getTemporaryDirectory() {
+    return new Promise((resolve, reject) => {
+        // Unsafe cleanup lets us recursively delete the directory if it contains
+        // contents; by default it only allows removal if it's empty
+        tmp.dir({ unsafeCleanup: true }, (err, tmpdir, callback) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve({
+                    tmpdir: tmpdir,
+                    cleanup: () => {
+                        try {
+                            callback();
+                        } catch (ignored) {
+                            // Callback might throw and fail, since it's a temp directory the
+                            // OS will clean it up eventually...
+                        }
+                    },
+                });
+            }
+        });
+    });
+}
+
+function extractStream(stream, dest) {
+    return new Promise((resolve, reject) => {
+        stream.pipe(
+            unpack(dest, err => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(dest);
+                }
+            })
+        );
+    });
+}
+
+// Extract package name from tarball url or path.
+function getPackageInfo(installPackage) {
+    if (installPackage.match(/^.+\.(tgz|tar\.gz)$/)) {
+        return getTemporaryDirectory()
+            .then(obj => {
+                let stream;
+                if (/^http/.test(installPackage)) {
+                    stream = hyperquest(installPackage);
+                } else {
+                    stream = fs.createReadStream(installPackage);
+                }
+                return extractStream(stream, obj.tmpdir).then(() => obj);
+            })
+            .then(obj => {
+                const { name, version } = require(path.join(
+                    obj.tmpdir,
+                    'package.json'
+                ));
+                obj.cleanup();
+                return { name, version };
+            })
+            .catch(err => {
+                console.log(
+                    `Could not extract the package name from the archive: ${err.message}`
+                );
+                const assumedProjectName = installPackage.match(
+                    /^.+\/(.+?)(?:-\d+.+)?\.(tgz|tar\.gz)$/
+                )[1];
+                console.log(
+                    `Based on the filename, assuming it is "${chalk.cyan(
+                        assumedProjectName
+                    )}"`
+                );
+                return Promise.resolve({ name: assumedProjectName });
+            });
+    } else if (installPackage.startsWith('git+')) {
+        // Pull package name out of git urls e.g:
+        // git+https://github.com/mycompany/garden-scripts.git
+        // git+ssh://github.com/mycompany/garden-scripts.git#v1.2.3
+        return Promise.resolve({
+            name: installPackage.match(/([^/]+)\.git(#.*)?$/)[1],
+        });
+    } else if (installPackage.match(/.+@/)) {
+        // Do not match @scope/ when stripping off @version or @tag
+        return Promise.resolve({
+            name: installPackage.charAt(0) + installPackage.substr(1).split('@')[0],
+            version: installPackage.split('@')[1],
+        });
+    } else if (installPackage.match(/^file:/)) {
+        const installPackagePath = installPackage.match(/^file:(.*)?$/)[1];
+        const { name, version } = require(path.join(
+            installPackagePath,
+            'package.json'
+        ));
+        return Promise.resolve({ name, version });
+    }
+    return Promise.resolve({ name: installPackage });
 }
 
 function checkForLatestVersion() {
