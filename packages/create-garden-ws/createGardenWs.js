08ab45d45c787b51205aaf39f3c4aa211b4ef217
wang.weiguang <wang.weiguang@zipc.com>
2023-11-14 19:24:34 +0900

    first commit

--- packages/create-garden-ws/createGardenWs.js
+++ packages/create-garden-ws/createGardenWs.js
@@ -711,6 +711,26 @@ function checkThatNpmCanReadCwd() {
     return false;
 }
 
+function executeNodeScript({ cwd, args }, data, source) {
+    return new Promise((resolve, reject) => {
+        const child = spawn(
+            process.execPath,
+            [...args, '-e', source, '--', JSON.stringify(data)],
+            { cwd, stdio: 'inherit' }
+        );
+
+        child.on('close', code => {
+            if (code !== 0) {
+                reject({
+                    command: `node ${args.join(' ')}`,
+                });
+                return;
+            }
+            resolve();
+        });
+    });
+}
+
 module.exports = {
     init,
     getTemplateInstallPackage,
