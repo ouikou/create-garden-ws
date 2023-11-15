cf8af710d7ea3c57eb53349ee396f5b19aa6a4c7
wang.weiguang <wang.weiguang@zipc.com>
2023-11-15 12:40:28 +0900

    first commit

--- packages/create-garden-ws/createGardenWs.js
+++ packages/create-garden-ws/createGardenWs.js
@@ -330,6 +330,32 @@ function run(
                     console.log(reason);
                 }
                 console.log();
+
+                // On 'exit' we will delete these files from target directory.
+                const knownGeneratedFiles = ['package.json', 'node_modules'];
+                const currentFiles = fs.readdirSync(path.join(root));
+                currentFiles.forEach(file => {
+                    knownGeneratedFiles.forEach(fileToMatch => {
+                        // This removes all knownGeneratedFiles.
+                        if (file === fileToMatch) {
+                            console.log(`Deleting generated file... ${chalk.cyan(file)}`);
+                            fs.removeSync(path.join(root, file));
+                        }
+                    });
+                });
+                const remainingFiles = fs.readdirSync(path.join(root));
+                if (!remainingFiles.length) {
+                    // Delete target folder if empty
+                    console.log(
+                        `Deleting ${chalk.cyan(`${appName}/`)} from ${chalk.cyan(
+                            path.resolve(root, '..')
+                        )}`
+                    );
+                    process.chdir(path.resolve(root, '..'));
+                    fs.removeSync(path.join(root));
+                }
+                console.log('Done.');
+                process.exit(1);
             })
     });
 }
